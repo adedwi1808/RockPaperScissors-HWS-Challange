76bf859d3ed54788fff2aa20a20543d941c60aa2
adedwi1808 <adedwip1808@gmail.com>
2022-10-04 17:14:32 +0700

    Initial Commit

---
+++ RockPaperScissors-HWS-Challange/RockPaperScissors_HWS_ChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissors_HWS_ChallangeApp.swift
+//  RockPaperScissors-HWS-Challange
+//
+//  Created by Ade Dwi Prayitno on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissors_HWS_ChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
